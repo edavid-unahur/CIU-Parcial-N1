9bf0e17f106c72b1c8c00e700eeefbb53a623fed
Ezeqdavid <ezeq.david98@gmail.com>
2022-09-26 01:12:06 -0300

    init

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementsByClassName("navbar navbar-expand-lg bg-light")
+
+
+
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
